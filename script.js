c9d8fefbf02d335b66a97bc305be860525cd0e9e
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-02-26 07:28:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const m3u8VideoURL = "https://epg.provider.plex.tv/library/parts/5e20b730f2f8d5003d739db7-61aaa014784a2161d3caf7fd.m3u8?includeAllStreams=1&X-Plex-Product=Plex+Mediaverse&X-Plex-Token=PG8cze1ATcsgtYs_-Kxg"; // Replace with your actual M3U8 link
+
+function loadM3U8Video() {
+    const video = document.getElementById('bg-video');
+
+    if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(m3u8VideoURL);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = m3u8VideoURL;
+        video.play();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadM3U8Video);
+
 const audioFiles = [
     "https://stream.zeno.fm/q1n2wyfs7x8uv",
     "https://stream.zeno.fm/d42hdvx96zhvv",
@@ -8,61 +28,16 @@ const audioFiles = [
     "https://stream.zeno.fm/xnuifxjgpomvv"
 ];
 
-const audioDetails = [
-    {
-        title: "Rage Music Philippines",
-        artist: "Various Artists",
-        albumArt: "https://i.imgur.com/a3iXI35.png"
-    },
-    {
-        title: "Clubmix",
-        artist: "DJ Clubmix",
-        albumArt: "https://i.imgur.com/mtxns7R.png"
-    },
-    {
-        title: "Emotions",
-        artist: "Chill Vibes",
-        albumArt: "https://i.imgur.com/nOBT9dU.png"
-    },
-    {
-        title: "Scream Radio",
-        artist: "Rock Legends",
-        albumArt: "https://i.imgur.com/WhCxsNJ.png"
-    },
-    {
-        title: "Vibe Radio",
-        artist: "Vibe Masters",
-        albumArt: "https://i.imgur.com/JbkwiBQ.png"
-    },
-    {
-        title: "Hot Hiphop And RnB",
-        artist: "HipHop Stars",
-        albumArt: "https://i.imgur.com/nfFWyYR.png"
-    },
-    {
-        title: "iMix Radio",
-        artist: "Electronic Beats",
-        albumArt: "https://i.imgur.com/CLD1X1t.png"
-    }
-];
-
 let currentAudio = new Audio();
 
 function playAudio(index) {
     if (!audioFiles[index]) return;
 
-    // Stop and reset the current audio
     if (!currentAudio.paused) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
     }
 
-    // Set new audio source and play
     currentAudio.src = audioFiles[index];
     currentAudio.play();
-
-    // Update the info section
-    document.getElementById("track-title").textContent = audioDetails[index].title;
-    document.getElementById("artist-name").textContent = audioDetails[index].artist;
-    document.getElementById("album-art").src = audioDetails[index].albumArt;
 }
